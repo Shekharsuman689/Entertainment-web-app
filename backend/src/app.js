302693bde6382eefdb4c62ac4696af1b8d95b8eb
Shekhar <shekharsuman689@gmail.com>
2024-05-09 12:14:09 +0530

    url

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://entertainment-web-app-orpin.vercel.app"],
+    origin: ["https://entertainment-web-app-orpin.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
